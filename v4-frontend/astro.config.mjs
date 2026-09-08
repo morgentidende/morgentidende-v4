6b2782c7d0e69064d2b32464658b5af9c5286906
morgentidende <325890484+morgentidende@users.noreply.github.com>
2026-09-08 12:49:27 +0200

    Avoid locking canonical host before domain launch

--- v4-frontend/astro.config.mjs
+++ v4-frontend/astro.config.mjs
@@ -3,6 +3,5 @@ import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
-  site: 'https://morgentidende.dk'
+  adapter: cloudflare()
 });
